885ffe7a024a64f7e37968713d93c8fa22e37367
Vatsal <bhesaniyavatsal@gmail.com>
2025-01-12 19:20:58 +0530

    Added animated list

---
+++ Animated List/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:animated_list/animated_list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animated List',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const AnimatedListWidget(),
+    );
+  }
+}
