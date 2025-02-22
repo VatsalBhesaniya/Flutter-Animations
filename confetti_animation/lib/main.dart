ed538fba2709f5080aa925b611ac9948a1f913ba
Vatsal <bhesaniyavatsal@gmail.com>
2025-02-22 08:18:58 +0530

    Added confetti animation

---
+++ confetti_animation/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:confetti_animation/confetti_animation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Confetti Animation',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ConfettiAnimation(),
+    );
+  }
+}
