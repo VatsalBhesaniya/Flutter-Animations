84ab32748cbac81e82a3c1fad6bcf2caa5c9600a
Vatsal <bhesaniyavatsal@gmail.com>
2025-08-11 10:32:27 +0530

    Added water ripple animation

---
+++ water_ripple_animation/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:water_ripple_animation/water_ripple_animation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Water Ripple Animation',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: WaterRippleAnimation(),
+    );
+  }
+}
